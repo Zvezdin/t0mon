1519ae7f8ecf63630bea647243e60be2e575a67e
zvezdin <git@zvezd.in>
2017-09-10 12:56:50 +0200

    Setup PrimeNG & Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 
+import { InputTextModule, ButtonModule, DataTableModule, DialogModule, DropdownModule }  from 'primeng/primeng';
+
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { JobComponent } from './job/job.component';
+import { HostsComponent } from './hosts/hosts.component';
+import { HostComponent } from './host/host.component';
 
 const appRoutes: Routes = [
 	{ path: 'home', component: HomeComponent },
+	{ path: 'jobs', component: JobsComponent },
+	{ path: 'hosts', component: HostsComponent },
 	//{ path: 'hero/:id',      component: HeroDetailComponent },
 	/*{
 	  path: 'heroes',
@@ -22,20 +33,28 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  declarations: [
-	AppComponent,
-	HomeComponent
-  ],
-  imports: [
-	BrowserModule,
-	HttpClientModule,
-	RouterModule.forRoot(
-		appRoutes,
-		{ enableTracing: false } // <-- if true, for debugging purposes only
-	)
-  
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		JobsComponent,
+		JobComponent,
+		HostsComponent,
+		HostComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		HttpClientModule,
+		RouterModule.forRoot(
+			appRoutes,
+			{ enableTracing: false } // <-- if true, for debugging purposes only
+		),
+
+		InputTextModule, ButtonModule, DataTableModule, DialogModule, DropdownModule,
+	
+		MdButtonModule, MdCheckboxModule,
+	],
+		providers: [],
+		bootstrap: [AppComponent]
 })
 export class AppModule { }
