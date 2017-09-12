d1d895aad35e96d3a90c24a934ec5ac631d5fb03
zvezdin <git@zvezd.in>
2017-09-12 11:50:30 +0200

    Added new pipe module to iterate key-value in ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule }   from '@angular/forms';
 
-import { InputTextModule, ButtonModule, DataTableModule, DialogModule, DropdownModule }  from 'primeng/primeng';
+import { InputTextModule, ButtonModule, DataTableModule, DialogModule, DropdownModule, MultiSelectModule }  from 'primeng/primeng';
 
-import {MdButtonModule, MdCheckboxModule} from '@angular/material';
+import {MdButtonModule, MdCheckboxModule, MdCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,11 +15,14 @@ import { JobsComponent } from './jobs/jobs.component';
 import { JobComponent } from './job/job.component';
 import { HostsComponent } from './hosts/hosts.component';
 import { HostComponent } from './host/host.component';
+import { KeysPipe } from './keys.pipe';
 
 const appRoutes: Routes = [
 	{ path: 'home', component: HomeComponent },
 	{ path: 'jobs', component: JobsComponent },
 	{ path: 'hosts', component: HostsComponent },
+	{ path: 'job/:id', component: JobComponent },
+	{ path: 'host/:id', component: HostComponent },
 	//{ path: 'hero/:id',      component: HeroDetailComponent },
 	/*{
 	  path: 'heroes',
@@ -39,20 +43,22 @@ const appRoutes: Routes = [
 		JobsComponent,
 		JobComponent,
 		HostsComponent,
-		HostComponent
+		HostComponent,
+		KeysPipe
 	],
 	imports: [
 		BrowserModule,
 		BrowserAnimationsModule,
 		HttpClientModule,
+		FormsModule,
 		RouterModule.forRoot(
 			appRoutes,
 			{ enableTracing: false } // <-- if true, for debugging purposes only
 		),
 
-		InputTextModule, ButtonModule, DataTableModule, DialogModule, DropdownModule,
+		InputTextModule, ButtonModule, DataTableModule, DialogModule, DropdownModule, MultiSelectModule,
 	
-		MdButtonModule, MdCheckboxModule,
+		MdButtonModule, MdCheckboxModule, MdCardModule,
 	],
 		providers: [],
 		bootstrap: [AppComponent]
