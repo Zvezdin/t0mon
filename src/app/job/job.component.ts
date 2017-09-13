0737673994862df959d3c5db417d7403078c9ea5
zvezdin <git@zvezd.in>
2017-09-13 23:34:18 +0200

    Updated job page to use the new txt-reader component

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -9,9 +9,8 @@ import { Location } from '@angular/common';
 	styleUrls: ['./job.component.css']
 })
 export class JobComponent implements OnInit {
-	error: boolean = false; 
-	jobFile: string = "Loading job file...";
-	job: string = "";
+	id: string = "";
+	path: string = "data/jobs/"
 
 	constructor(
 		private route: ActivatedRoute,
@@ -23,20 +22,8 @@ export class JobComponent implements OnInit {
 	ngOnInit() {
 		this.route.params.subscribe(params => {
 			if(params["id"] != undefined){
-				this.getJob(params["id"]);
-				this.job = params["id"];
+				this.id = params["id"];
 			}
-			else this.error = true;
 		});
 	}
-
-	getJob(job: string){
-
-		this.http.get('data/jobs/'+job, {responseType: 'text'} ).subscribe(data => {
-			this.jobFile = data;
-		}, err => {
-			this.error = true;
-		});
-	}
-
 }
