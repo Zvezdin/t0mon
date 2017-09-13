f20b9599408f171585b50599acf7b3d336253795
zvezdin <git@zvezd.in>
2017-09-13 23:34:41 +0200

    Created host component to display host details

--- src/app/host/host.component.ts
+++ src/app/host/host.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http'
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-host',
-  templateUrl: './host.component.html',
-  styleUrls: ['./host.component.css']
+	selector: 'app-host',
+	templateUrl: './host.component.html',
+	styleUrls: ['./host.component.css']
 })
 export class HostComponent implements OnInit {
+	id: string = "";
+	path: string = "data/hosts/"
 
-  constructor() { }
+	constructor(
+		private route: ActivatedRoute,
+		private location: Location,
+		private router: Router,
+		private http: HttpClient,
+	) { }
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.route.params.subscribe(params => {
+			if(params["id"] != undefined){
+				this.id = params["id"];
+			}
+		});
+	}
 }
