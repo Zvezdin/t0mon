19d08dd32d805b8ea1cf617551209d1e22fec70b
zvezdin <git@zvezd.in>
2017-09-14 18:20:10 +0200

    Created hosts component to display hosts.txt interactively

--- src/app/hosts/hosts.component.ts
+++ src/app/hosts/hosts.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-hosts',
-  templateUrl: './hosts.component.html',
-  styleUrls: ['./hosts.component.css']
+	selector: 'app-hosts',
+	templateUrl: './hosts.component.html',
+	styleUrls: ['./hosts.component.css']
 })
 export class HostsComponent implements OnInit {
 
-  constructor() { }
+	filterFields = ['STATUS', 'MAX']; //the fields for which we want to filter
+	//HOST_NAME          STATUS       JL/U    MAX  NJOBS    RUN  SSUSP  USUSP    RSV 
+	columnLabels = {HOST_NAME: 'Host name', STATUS: 'Status', 'JL/U': 'JL/U', MAX: 'Max', NJOBS: 'N Jobs', RUN: 'Run', SSUSP: 'SSUSP', USUSP: 'USUSP', RSV: 'RSV'};
+	buttonData = [{label: 'Host details', url: '/host', field: 'HOST_NAME'}];
+	columnStyles = {};
+	tableLabel = "List of hosts";
+	filepath = 'data/hosts/hosts.txt';
+	baseURL = '/hosts';
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
 
 }
