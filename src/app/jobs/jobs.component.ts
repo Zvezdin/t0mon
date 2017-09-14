2d1bb1140a0f3d0ca075a0a22c05d88e409d47df
zvezdin <git@zvezd.in>
2017-09-14 17:05:52 +0200

    Rewrote jobs to be more dynamic and automatic

--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
 import {  ActivatedRoute, Params, Router, NavigationExtras } from '@angular/router';
 import { Location } from '@angular/common';
@@ -9,28 +9,32 @@ import { SelectItem } from 'primeng/primeng';
 
 import * as $ from 'jquery';
 
+
+interface SelectedItems {'USER': string[], 'STAT': string[], 'QUEUE': string[], 'FROM_HOST': string[]};
+
+
 @Component({
 	selector: 'app-jobs',
 	templateUrl: './jobs.component.html',
 	styleUrls: ['./jobs.component.css']
 })
 
-
 export class JobsComponent implements OnInit {
 	@ViewChild(DataTable)
 	private table: DataTable; //here we will store the variable for the datatable.
 
-	jobs: Array<{/* jobID: string, user: string, stat: string, queue: string, fromHost: string, execHost: string, jobName: string, submitTime: string*/}>;
+	rows: Array<{}>;
 
-	private users: SelectItem[];
-	private statuses: SelectItem[];
-	private queues: SelectItem[];
-	private CEs: SelectItem[];
+	private items: any;
+	
+	private selectedItems: any;
 
-	private selectedUsers: string[];
-	private selectedStatuses: string[];
-	private selectedQueues: string[];
-	private selectedCEs: string[];
+	@Input() filterFields = ['USER', 'STAT', 'QUEUE', 'FROM_HOST']; //the fields for which we want to filter
+	@Input() columnLabels = {JOBID: 'Job ID', USER: 'User', STAT: 'Status', QUEUE: 'Queue', FROM_HOST: 'CE', EXEC_HOST: 'Exec host', CORES: 'Cores', JOB_NAME: 'Job name', SUBMIT_TIME: 'Submit time'};
+	@Input() buttonData = [{label: 'Job details', url: '/job', field: 'JOBID'}, {label: 'Host details', url: '/host', field: 'EXEC_HOST'}];
+	@Input() columnStyles = {FROM_HOST: "{'width':'200px'}"};
+	@Input() private filepath = 'data/jobs/jobs.txt';
+	@Input() private baseURL = '/jobs';
 
 	constructor(
 		private route: ActivatedRoute,
@@ -40,97 +44,81 @@ export class JobsComponent implements OnInit {
 	) { }
 
 	ngOnInit() {
-		this.loadJobs(this.onJobsLoaded);
+		this.selectedItems = {};
+		this.items = {};
+
+		for(var i=0; i<this.filterFields.length; i++){
+			this.selectedItems[this.filterFields[i]] = [];
+			this.items[this.filterFields[i]] = [];
+		}
+
+		this.loadData(this.onDataLoaded);
 	}
 
-	loadJobs(callback){
-		this.http.get('data/jobs/jobs.txt', {responseType: 'text'} ).subscribe(data => {
+	loadData(callback){
+		this.http.get(this.filepath, {responseType: 'text'} ).subscribe(data => {
 			callback(data);
 		});
 	}
 
-	onJobsLoaded = (data:string) => {
-		var jobLines: string[] = data.split(/\r?\n/); //split the file into lines
+	onDataLoaded = (data:string) => {
+		var lines: string[] = data.split(/\r?\n/); //split the file into lines
 
-		console.log(jobLines[0].split(/[ \t]+/), jobLines[1].split(/[ \t]+/));
+		console.log(lines[0].split(/[ \t]+/), lines[1].split(/[ \t]+/)); //debug
 
-		var header = jobLines[0].split(/[ \t]+/); //the header, containing the names of the values
+		var header = lines[0].split(/[ \t]+/); //the header, containing the names of the values
 
-		var jobs = new Array();
+		var rows = new Array(); //the datatable rows
 
-		var users = {};
-		var statuses = {};
-		var queues = {};
-		var CEs = {};
+		var items = {}; //used to generate the item list for UI multiselect filters
 
-		this.users = [];
-		this.statuses = [];
-		this.queues = [];
-		this.CEs = [];
+		for(var i =0; i<this.filterFields.length; i++) items[this.filterFields[i]] = {};
 
-		for(var i=1; i<jobLines.length; i++){
-			var line = jobLines[i];
+		for(var i=1; i<lines.length; i++){
+			var line = lines[i];
 
 			if(line == undefined || line == "") continue;
 
 			var values = line.split(/[ \t]+/); //split the line into values, separated by space or a tab.
 
-			var job = {};
+			var row = {};
 			for(var j=0; j< values.length; j++){
 				if(j < header.length){
 
-					if(header[j] == 'EXEC_HOST'){ //if we are processing an exec_host, we need to do a bit more than just adding it.
+					if(header[j] == 'EXEC_HOST'){ //if we are processing a field type exec_host, we need to do a bit more than just adding it.
 						var hosts = values[j].split(":");
-						job[header[j]] = hosts[0]; //only the unique host ID
-						job["CORES"] = hosts.length;
+						row[header[j]] = hosts[0]; //only the unique host ID
+						row["CORES"] = hosts.length;
 					}
-					else job[header[j]] = values[j];
+					else row[header[j]] = values[j];
 
 					var selectItem = {label: values[j], value: values[j]}; //create a selectItem, which we MAY insert into one of our select dropdowns.
 
+					for(var k=0; k<this.filterFields.length; k++){
+						var field = this.filterFields[k];
 
-					if(header[j] == 'USER'){ //if the current value is a USER
-						if(!users.hasOwnProperty(values[j])){ //if we haven't added that user yet
-							users[values[j]] = true; //mark that user as added
+						if(header[j] == field){
+							if(!items[field].hasOwnProperty(values[j])){ //if we haven't added that value yet
+								items[field][values[j]] = true; //mark that value as added
 
-							this.users.push(selectItem); //add that user to the select dropdown.
-						}
-					}
-					if(header[j] == 'STAT'){ //if the current value is a status
-						if(!statuses.hasOwnProperty(values[j])){
-							statuses[values[j]] = true;
-
-							this.statuses.push(selectItem);
-						}
-					}
-					if(header[j] == 'QUEUE'){
-						if(!queues.hasOwnProperty(values[j])){
-							queues[values[j]] = true;
-
-							this.queues.push(selectItem);
-						}
-					}
-					if(header[j] == 'FROM_HOST'){
-						if(!CEs.hasOwnProperty(values[j])){
-							CEs[values[j]] = true;
-
-							this.CEs.push(selectItem);
+								this.items[field].push(selectItem); //add that value to the select dropdown.
+							}
 						}
 					}
 				}
 				else {
-					job[header[header.length-1]] += " " + values[j]; //the last value of each line - SUBMIT_TIME is split into multiple segments, since the date in the data is separated by spaces. This parses the date correctly.
+					row[header[header.length-1]] += " " + values[j]; //the last value of each line - SUBMIT_TIME is split into multiple segments, since the date in the data is separated by spaces. This parses the date correctly.
 				}
 			}
 
-			jobs.push(job);
+			rows.push(row);
 		}
 
-		this.jobs = jobs;
-		
-		var self = this;
+		this.rows = rows;
 
-		setTimeout(() => {
+		console.log(this.items); //debug
+
+		setTimeout(() => { //we need to wait one tick before we can parse the route and filter the table
 			this.parseRoute();
 		})
 	}
@@ -139,20 +127,13 @@ export class JobsComponent implements OnInit {
 		this.route.queryParams.subscribe(params => {
 			console.log("Route parameters: ", params);
 
-			if(params.hasOwnProperty('statuses')){
-				this.selectedStatuses = params['statuses'].split(",");
+			for(var field in this.selectedItems){
+				if(params.hasOwnProperty(field)){
+					this.selectedItems[field] = params[field].split(",");
+				}
 			}
 
-			if(params.hasOwnProperty('users')){
-				this.selectedUsers = params['users'].split(",");
-			}
-
-			if(params.hasOwnProperty('queues')){
-				this.selectedQueues = params['queues'].split(",");
-			}
-			if(params.hasOwnProperty('CEs')){
-				this.selectedCEs = params['CEs'].split(",");
-			}
+			console.log(this.selectedItems);
 
 			this.updateTable();
 		});
@@ -163,49 +144,29 @@ export class JobsComponent implements OnInit {
 			queryParams: {},
 		};
 	  
-		if(this.selectedUsers != undefined && this.selectedUsers.length > 0){
-			params.queryParams['users'] = this.selectedUsers.join(',');
+		for(var field in this.selectedItems){
+			if(this.selectedItems[field] != undefined && this.selectedItems[field].length > 0){
+				params.queryParams[field] = this.selectedItems[field].join(',');
+			}
 		}
 
-		if(this.selectedStatuses != undefined && this.selectedStatuses.length > 0){
-			params.queryParams['statuses'] = this.selectedStatuses.join(',');
-		}
-
-		if(this.selectedQueues != undefined && this.selectedQueues.length > 0){
-			params.queryParams['queues'] = this.selectedQueues.join(',');
-		}
-		if(this.selectedCEs != undefined && this.selectedCEs.length > 0){
-			params.queryParams['CEs'] = this.selectedCEs.join(',');
-		}
-
-		// Navigate to the login page with extras
-		this.router.navigate(['/jobs'], params);
+		//Update the actual route
+		this.router.navigate([this.baseURL], params);
 
 		console.log("Route updated")
 	}
 
 	updateTable(){
-		this.sortTable(this.selectedUsers, "USER");
-		this.sortTable(this.selectedQueues, "QUEUE");
-		this.sortTable(this.selectedStatuses, "STAT");
-		this.sortTable(this.selectedCEs, "FROM_HOST");
+		for(var field in this.selectedItems){
+			this.sortTable(this.selectedItems[field], field);
+		}
 	}
 
 	sortTable(selectedItems: string[], field: string, filterMatchMode: string = "in"){
 		this.table.filter(selectedItems, field, filterMatchMode);
 	}
 
-	gotoJobDetail(job){
-		console.log(job, this.selectedUsers, this.selectedQueues, this.selectedStatuses);
-		this.router.navigate(['/job', job]);
-	}
-
-	gotoHostDetail(host){
-		console.log(host);
-		this.router.navigate(['/host', host]);
-	}
-
-	printDebug(a: Array<any>){
-		console.log(a);
+	gotoDetail(path: string, file: string){
+		this.router.navigate([path, file]);
 	}
 }
